a9995a96a742786df4762cc5f3ba56a6663fb4d5
Shubham Bharti <shubhambharti94@gmail.com>
2017-10-29 18:13:24 +0530

    Website Change
    Hopefully this will be the last change

---
+++ js/portfolio.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){jQuery('.gallery').mixItUp()});var elem=document.querySelector('.m-p-g');document.addEventListener('DOMContentLoaded',function(){new MaterialPhotoGallery(elem)});
